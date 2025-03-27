a37dc5502c7b82f3f1b32af86a53a5b9e4d9e5ba
sanghak yi <sang105kr@gmail.com>
2025-03-27 17:48:04 +0900

    250327-8-2

--- src/day6/Question_4.java
+++ src/day6/Question_4.java
@@ -1,4 +1,7 @@
 package day6;
+
+import java.util.Scanner;
+
 /*
   배열의 요소 검색
   설명: 정수형 배열에서 특정 요소를 검색하는 메소드를 작성하시오.
@@ -10,12 +13,43 @@ package day6;
   정수3 : 30
   검색할 값 : 20
   인덱스 : 1
-  
+
   검색할 값 : 50
   인덱스 : 배열에 존재하지 않습니다.
  */
 public class Question_4 {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("배열의 크기 : ");
+    int sizeOfArray = scanner.nextInt();
+    //배열 생성
+    int[] array = new int[sizeOfArray];
 
+    //배열 크기의 정수 입력 받기
+    for (int i = 0; i < array.length; i++) {
+      System.out.printf("정수%d : ", (i+1) );
+      array[i] = scanner.nextInt();
+    }
+
+    System.out.print("검색할 값 : ");
+    int nubmer = scanner.nextInt();
+
+    int findedIndex = findIndex(array, nubmer);
+    if(findedIndex == -1){
+      System.out.println("인덱스 : 배열에 존재하지 않습니다.");
+    }else{
+      System.out.println("인덱스 : " + findedIndex);
+    }
+  }
+
+  public static int findIndex(int[] array, int number) {
+    int index = -1;
+    for (int i = 0; i < array.length; i++) {
+      if( number == array[i]){
+        index = i;
+        break;
+      }
+    }
+    return index;
   }
 }
